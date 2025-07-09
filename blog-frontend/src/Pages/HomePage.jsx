be6bee640220b24f3867709fd182d67c67123a49
Bahar Şahin <bahar.shn04@gmail.com>
2025-07-09 15:50:55 +0300

    frontend kuruldu

---
+++ blog-frontend/src/Pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function HomePage() {
+    return (
+        <div style={{ padding: '2rem', textAlign: 'center' }}>
+            <h1>Welcome to My Blog Home Page</h1>
+            <p>This is a simple demo of the Home Page component!</p>
+        </div>
+    );
+}
